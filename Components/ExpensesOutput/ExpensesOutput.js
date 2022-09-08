a8b58aaea55380ac3be946a4bc4ee3a30dd188df
Kashyap2424 <kapdikashyap2003@gmail.com>
2022-09-08 17:03:39 +0530

    Working on Expense List Items

--- Components/ExpensesOutput/ExpensesOutput.js
+++ Components/ExpensesOutput/ExpensesOutput.js
@@ -2,6 +2,7 @@ import { StyleSheet, View } from "react-native";
 import React from "react";
 import ExpensesSummary from "./ExpensesSummary";
 import ExpensesList from "./ExpensesList";
+import { GlobalStyles } from "../../utils/styles";
 
 const DUMMY_EXPENSES = [
   {
@@ -38,7 +39,7 @@ const DUMMY_EXPENSES = [
 
 const ExpensesOutput = ({ expenses, expensesPeriodName }) => {
   return (
-    <View>
+    <View style={styles.container}>
       <ExpensesSummary
         expenses={DUMMY_EXPENSES}
         periodName={expensesPeriodName}
@@ -50,4 +51,10 @@ const ExpensesOutput = ({ expenses, expensesPeriodName }) => {
 
 export default ExpensesOutput;
 
-const styles = StyleSheet.create({});
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    paddingHorizontal: 12,
+    paddingTop: 12,
+  },
+});
