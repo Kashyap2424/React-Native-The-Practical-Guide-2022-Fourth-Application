7ddc2d56a532f3265c7465e8758b3047bae73be6
Kashyap2424 <kapdikashyap2003@gmail.com>
2022-09-09 12:06:10 +0530

    Adding Validation

--- Components/ManageExpenses/ExpensesForm.js
+++ Components/ManageExpenses/ExpensesForm.js
@@ -1,5 +1,5 @@
 import { useState } from "react";
-import { StyleSheet, Text, View } from "react-native";
+import { StyleSheet, Text, View, Alert } from "react-native";
 
 import Input from "./Input";
 import Button from "../Ui/Button";
@@ -29,6 +29,15 @@ const ExpensesForm = ({
       date: new Date(inputValue.date),
       description: inputValue.description,
     };
+
+    const amountIsValid = !isNaN(expenseData.amount) && expenseData.amount > 0;
+    const dateIsValid = expenseData.date.toString() !== "Invalid Date";
+    const descriptionIsValid = expenseData.description.trim().length > 0;
+
+    if (!amountIsValid || !dateIsValid || !descriptionIsValid) {
+      Alert.alert("Invalid input", "Please check your input values");
+      return;
+    }
 
     onSubmit(expenseData);
   }
