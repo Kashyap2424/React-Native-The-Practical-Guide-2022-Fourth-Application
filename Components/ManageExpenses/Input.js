8da87bcc4778083bd038ae5978b4426e6c18c097
Kashyap2424 <kapdikashyap2003@gmail.com>
2022-09-09 11:14:41 +0530

    Adding Styling

--- Components/ManageExpenses/Input.js
+++ Components/ManageExpenses/Input.js
@@ -1,14 +1,43 @@
 import { StyleSheet, Text, View, TextInput } from "react-native";
 
+import { GlobalStyles } from "../../utils/styles";
+
 const Input = ({ label, textInputConfig }) => {
+  const inputStyles = [styles.input];
+
+  if (textInputConfig && textInputConfig.multiline) {
+    inputStyles.push(styles.inputMultiline);
+  }
+
   return (
-    <View>
-      <Text>{label}</Text>
-      <TextInput {...textInputConfig} />
+    <View style={styles.inputContainer}>
+      <Text style={styles.label}>{label}</Text>
+      <TextInput style={inputStyles} {...textInputConfig} />
     </View>
   );
 };
 
 export default Input;
 
-const styles = StyleSheet.create({});
+const styles = StyleSheet.create({
+  inputContainer: {
+    marginHorizontal: 4,
+    marginVertical: 8,
+  },
+  label: {
+    fontSize: 12,
+    color: GlobalStyles.colors.primary700,
+    marginBottom: 4,
+  },
+  input: {
+    backgroundColor: GlobalStyles.colors.primary50,
+    color: GlobalStyles.colors.primary800,
+    padding: 8,
+    borderRadius: 6,
+    fontSize: 18,
+  },
+  inputMultiline: {
+    minHeight: 100,
+    textAlignVertical: "top",
+  },
+});
